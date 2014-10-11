f919436d4ac9a4eaa4a15231bdabf53ced02435c
Ben Lau <xbenlau@gmail.com>
2014-10-11 23:50:22 +0800

    Added QuickAndroid::dp() function for C++ code to obtain the DP value.

--- quickandroid.cpp
+++ quickandroid.cpp
@@ -8,23 +8,23 @@
 #endif
 
 //static QVariantMap data;
-static qreal dp = 1;
-static qreal dpi = 72;
+static qreal m_dp = 1;
+static qreal m_dpi = 72;
 
 static QJSValue aProvider(QQmlEngine *engine, QJSEngine *scriptEngine)
 {
     Q_UNUSED(engine);
 
     QJSValue value = scriptEngine->newObject();
-    value.setProperty("dp",dp);
-    value.setProperty("dpi",dpi);
+    value.setProperty("dp",m_dp);
+    value.setProperty("dpi",m_dpi);
 
     return value;
 }
 
 void QuickAndroid::registerTypes()
 {
-    Q_UNUSED(dpi);
+    Q_UNUSED(m_dpi);
 #ifdef Q_OS_ANDROID
     QAndroidJniObject activity = QAndroidJniObject::callStaticObjectMethod("org/qtproject/qt5/android/QtNative", "activity", "()Landroid/app/Activity;");
     QAndroidJniObject resource = activity.callObjectMethod("getResources","()Landroid/content/res/Resources;");
@@ -33,4 +33,9 @@ void QuickAndroid::registerTypes()
     dpi = metrics.getField<int>("densityDpi");
 #endif
     qmlRegisterSingletonType("QuickAndroid", 0, 1, "A", aProvider);
+}
+
+qreal QuickAndroid::dp() const
+{
+    return m_dp;
 }
