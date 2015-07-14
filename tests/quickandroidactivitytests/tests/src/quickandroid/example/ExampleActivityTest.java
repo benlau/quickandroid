6399ff8c758ed1939e9259bd53c0f2da7ac39e7c
Ben Lau <xbenlau@gmail.com>
2015-07-15 01:56:23 +0800

    Add reentrant tests

--- tests/quickandroidactivitytests/tests/src/quickandroid/example/ExampleActivityTest.java
+++ tests/quickandroidactivitytests/tests/src/quickandroid/example/ExampleActivityTest.java
@@ -7,6 +7,10 @@ import java.util.Map;
 import android.content.Intent;
 import android.test.ActivityTestCase;
 import android.app.Instrumentation;
+import android.app.Activity;
+import java.util.Queue;
+import java.util.LinkedList;
+
 /**
  * This is a simple framework for a test of an Application.  See
  * {@link android.test.ApplicationTestCase ApplicationTestCase} for more information on
@@ -25,9 +29,26 @@ public class ExampleActivityTest extends ActivityInstrumentationTestCase2<Exampl
         super("quickandroid.example", ExampleActivity.class);
     }
 
-    @Override
-    protected void setUp() throws Exception {
-           super.setUp();
+    private static boolean launched = false;
+    private static Activity mActivity = null;
+
+    private void startActivity() {
+//        Log.v(TAG,String.format("Pre StartActivity: %b",launched));
+
+        if (launched)
+            return;
+
+        Log.v(TAG,"StartActivity");
+
+        Instrumentation instrumentation = getInstrumentation();
+        Intent intent = new Intent(getInstrumentation()
+                .getTargetContext(), ExampleActivity.class);
+        intent.setFlags(intent.getFlags()  | Intent.FLAG_ACTIVITY_NEW_TASK);
+
+        mActivity = instrumentation.startActivitySync(intent);
+        launched = true;
+        Log.v(TAG,"End of StartActivity");
+
     }
 
     public void testHello() {
@@ -37,13 +58,8 @@ public class ExampleActivityTest extends ActivityInstrumentationTestCase2<Exampl
     private int counter = 0;
 
     public void testSendMessage() {
-
-        Instrumentation instrumentation = getInstrumentation();
-        Intent intent = new Intent(getInstrumentation()
-                .getTargetContext(), ExampleActivity.class);
-        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        instrumentation.startActivitySync(intent);
-
+        startActivity();
+        Log.v(TAG,"testSendMessage");
 
         SystemMessenger.Listener listener = new SystemMessenger.Listener() {
 
@@ -69,7 +85,42 @@ public class ExampleActivityTest extends ActivityInstrumentationTestCase2<Exampl
 
         SystemMessenger.post("testSendMessage",null);
         assertTrue(counter == 1);
+
     }
+
+    private Queue<String> queue;
+
+    public void testReentrant() {
+        startActivity();
+        queue = new LinkedList();
+
+        Log.v(TAG,"testReentrant");
+        final String messageName = "testReentrant";
+
+        SystemMessenger.Listener listener = new SystemMessenger.Listener() {
+
+            public boolean post(String name , Map data) {
+                if (name.equals("ping")) {
+                    counter++;
+                    SystemMessenger.post("pong");
+                    return true;
+                }
+                return false;
+            }
+        };
+
+        SystemMessenger.addListener(listener);
+        SystemMessenger.addListener(listener);
+
+        assertTrue(counter == 0);
+
+        SystemMessenger.post("ping",null);
+        assertEquals(counter , 6);
+
+        SystemMessenger.removeListener(listener);
+
+    }
+
 
 
 }
